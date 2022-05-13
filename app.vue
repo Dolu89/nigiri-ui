51b1c3916613ec1e825ab3a5ac40577f42368327
Dolu <doludev@protonmail.com>
2022-05-13 13:20:21 +0200

    Use grid system instead of custom flex

--- app.vue
+++ app.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="main">
-    <div class="row">
-      <Bitcoin class="w100" />
-      <BitcoinConnect class="w100" />
+  <div class="grid">
+    <div class="col-12 md:col-6">
+      <Bitcoin />
     </div>
-    <div class="row">
-      <Lnd class="w100" />
-      <Clightning class="w100" />
+    <div class="col-12 md:col-6">
+      <BitcoinConnect />
+    </div>
+    <div class="col-12 md:col-6">
+      <Lnd />
+    </div>
+    <div class="col-12 md:col-6">
+      <Clightning />
     </div>
   </div>
 </template>
@@ -18,18 +22,6 @@ body {
 </style>
 
 <style lang="scss" scoped>
-.main {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-
-.w100 {
-  width: 100%;
-}
-
-.row {
-  display: inline-flex;
-  gap: 20px;
+.grid {
 }
 </style>
